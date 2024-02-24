f3eadb169896022a1cdd6584ae557982b02ce78c
adam.falkowski <adam.falkowski@beumer.com>
2024-02-23 21:46:36 -0500

    change

--- xl.py
+++ xl.py
@@ -3,6 +3,6 @@ from openpyxl import Workbook, load_workbook
 wb = load_workbook('xl.xlsx')
 ws = wb.active
 
-wb.create_sheet("Test")
+wb.create_sheet("Testing")
 print(wb.sheetnames)
 wb.save('xl.xlsx')
