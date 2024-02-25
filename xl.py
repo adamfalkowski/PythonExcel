a99f7548be9453e68f6b043bc232d7b8e39777d8
adam.falkowski <adam.falkowski@beumer.com>
2024-02-24 20:15:01 -0500

    added defined name trasfer fucntion

--- xl.py
+++ xl.py
@@ -1,8 +1,32 @@
 from openpyxl import Workbook, load_workbook
+from openpyxl.workbook.defined_name import DefinedName
 
-wb = load_workbook('xl.xlsx')
-ws = wb.active
+# Project variables
+project_number = "6200000120"
 
-wb.create_sheet("Testing")
-print(wb.sheetnames)
-wb.save('xl.xlsx')
+
+
+
+def transfer_defined_names(original_workbook_name, new_workbook_name, new_worksheet_name, project_number):
+    original_workbook = load_workbook(original_workbook_name)
+    orignal_def_names = original_workbook.defined_names.definedName
+
+    new_workbook = Workbook()
+    new_workbook_sheet = new_workbook.active
+    new_workbook_sheet.title = new_worksheet_name
+
+    for name in orignal_def_names:
+        
+        if name.value != "#REF!":
+            first_value, second_value = name.value.split("!")
+            new_def_names = DefinedName(name=name.name,attr_text=f"{new_worksheet_name}!{second_value}")
+            new_workbook.defined_names.append(new_def_names)
+
+    new_workbook.save(f"{new_workbook_name}_{project_number}.xlsx")
+
+    return new_workbook
+
+transfer_defined_names("TagsEquipment_bgfcv_templete.xlsx","TagsEquipment", "Tags_Equipment",project_number)
+
+
+
